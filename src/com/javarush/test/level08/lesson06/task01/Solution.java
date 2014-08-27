7ba974d44a7c8c4130e873f96da517782afe41f3
garibaldi1 <haskeuda123>
2014-08-27 12:09:47 +0400

    Решена задача level08.lesson06.task01

---
+++ src/com/javarush/test/level08/lesson06/task01/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level08.lesson06.task01;
+
+import java.util.*;
+
+/* Создать два списка LinkedList и ArrayList
+Нужно создать два списка – LinkedList и ArrayList.
+*/
+
+public class Solution
+{
+    public static Object createArrayList()
+    {
+        ArrayList<Objects> arrayList = new ArrayList<Objects>();
+        return arrayList;//Напишите тут ваш код
+
+    }
+
+    public static Object createLinkedList()
+    {
+        LinkedList<Objects> linkedList = new LinkedList<Objects>();
+        return linkedList;
+        //Напишите тут ваш код
+
+    }
+}
