b3fa09abcd008e632b6757c7e05412b74f3b9380
garibaldi1 <haskeuda123>
2014-10-15 17:07:17 +0400

    Задача level07,lesson12,home03 - решено

---
+++ src/com/javarush/test/level07/lesson12/home03/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.test.level07.lesson12.home03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/* Максимальное и минимальное числа в массиве
+Создать массив на 20 чисел. Заполнить его числами с клавиатуры. Найти максимальное и минимальное числа в массиве.
+Вывести на экран максимальное и минимальное числа через пробел.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int  maximum = 0;
+        int  minimum = 0;
+        int[] list = new int[20];
+        for (int a = 0; a < 20; a++)
+        {
+            list[a] = Integer.parseInt(reader.readLine());
+        }
+        maximum = list[0];
+        minimum = list[0];
+        for (int i =0; i < list.length; i++)
+        {
+            if(list[i] >maximum)
+            {
+                maximum = list[i];
+            }
+            if(list[i]<minimum)
+            {
+                minimum = list[i];
+            }
+        }
+
+        //Напишите тут ваш код
+        System.out.println(maximum);
+        System.out.println(minimum);
+    }
+}
